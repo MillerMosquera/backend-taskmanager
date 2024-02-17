021a096582a675ed3bc978857bdbbe24d7caedc0
Miller Mosquera <millerpalacios16@gmail.com>
2024-02-17 15:02:07 -0500

    jwt configuration with spring security

--- src/main/java/com/example/backendtaskmanager/Repository/UserRepository.java
+++ src/main/java/com/example/backendtaskmanager/Repository/UserRepository.java
@@ -1,1 +1,1 @@
-package com.example.backendtaskmanager.Repository;import com.example.backendtaskmanager.Entity.User;import org.springframework.data.jpa.repository.JpaRepository;public interface UserRepository extends JpaRepository<User, Long>{}
+package com.example.backendtaskmanager.Repository;import com.example.backendtaskmanager.Entity.User;import org.springframework.data.jpa.repository.JpaRepository;import java.util.Optional;public interface UserRepository extends JpaRepository<User, Long>{    Optional<User> findUserByEmail(String email);}
---
+++ src/main/java/com/example/backendtaskmanager/Security/Payload/LoginRequest.java
@@ -0,0 +1,1 @@
+package com.example.backendtaskmanager.Security.Payload;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;import lombok.NoArgsConstructor;@Data@Builder@AllArgsConstructor@NoArgsConstructorpublic class LoginRequest {    private String email;    private String password;}
